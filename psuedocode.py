50fc4724805b7fd7bab611ea24da2167d81131d6
Severiano Villarruel <severianovillarruel@Severianos-MacBook-Pro.local>
2019-08-10 22:58:51 -0700

    don

--- psuedocode.py
+++ psuedocode.py
@@ -39,6 +39,7 @@
 #     reverse sort index2_seq_rev_comp
 #     retrun index2_seq_rev_comp
 
+#use with open("example.txt", "a") as text_file: to not overwrite outpufiles
 #index_ref_dict = {index_ref_seq:index_ref}
 #OUTPUT LOW QUALITY OR UNKNOWN READS
 #def id_weird_reads(index1_seq, read1_seq, index2_seq_rev_comp, read2_seq, index_ref_dict):
@@ -53,7 +54,6 @@
 #   if the index is not found in the dictionary
 #   1) add index1_seq and index1_ref and index2_seq and index2_ref to the header and write the forward seq out to the 'low_qual_and_unknown_forward'
 #   2) add index1_seq and index1_ref and index2_seq and index2_ref to the header and write the forward seq out to the 'low_qual_and_unknown_rev'
-#   return each of the output files
 
 #OUTPUT MISMATCHED READS
 #def hopped_read)sort(index1_seq, read1_seq, index2_seq, read2_seq, index_ref_dict)
@@ -61,7 +61,6 @@
 #   if index1_seq is a key in index_ref_dict and index2_rev_comp is a key in index_ref_dict but the index_seqs do not equal eachother
 #   1) add index1_seq and index1_ref and index2_seq and index2_ref to the header and write the forward seq out to a file called 'hopped_forward'
 #   2) add index1_seq and index1_ref and index2_seq and index2_ref to the header write the reverse seq out to a file called 'hopped_rev'
-#   return each of the output files
 
 #OUTPUT MATCHED READS
 #def dual_matched_sort(index1_seq, read1_seq, index2_seq_rev_comp, read2_seq, index_ref_dict)
@@ -69,4 +68,3 @@
 #   if index1_seq and index2_seq are keys in index_ref_dict and if index1_seq equals index2_seq
 #   1) add the index_seq and index_ref to the header and write the forward read out to a file named 'forward' + the index_ref
 #   2)  add the index_seq and index_ref to the header and write the reverse read out to a file named 'reverse' + the index_ref
-#   return each of the output files
